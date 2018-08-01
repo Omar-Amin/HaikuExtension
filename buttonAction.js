3a0592cd13f92795ac3e9e9c264246c99dd50143
Omar Amin <omar_75@hotmail.dk>
2018-08-01 21:37:11 +0200

    Appends top 50 videos to a list

--- buttonAction.js
+++ buttonAction.js
@@ -5,17 +5,31 @@
   //We get the data from top haiku
   //The data is written in json
   //Only visible in console (Look for 'Object')
-  async function fetchTopFive() {
+  var list = [];
+
+  async function fetchTop() {
   const URL = `https://www.reddit.com/r/youtubehaiku/top/.json?limit=50`;
-  const fetchResult = fetch(URL)
-  const response = await fetchResult;
-  if (response.ok) {
-    const jsonData = await response.json();
-    console.log(jsonData);
+  try {
+    const fetchResult = fetch(new Request(URL, { method: 'GET', cache: 'reload' }));
+    const response = await fetchResult;
+    if (response.ok) {
+      const jsonData = await response.json();
+      listOfVids(jsonData);
     } else {
       throw Error(response.statusText);
+      }
+    } catch (e) {
+      console.log(e);
     }
   }
 
-fetchTopFive();
+  //Takes each URL and appends it to the list
+  function listOfVids(json){
+    const posts = json.data.children;
+    return posts.map(post => list.push(post.data.url));
+  }
+
+  fetchTop();
+  console.log(list);
+
 })();
