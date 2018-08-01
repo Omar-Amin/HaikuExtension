930fbdd89f56417f3c6baae6fd644fb267acd3bd
Omar Amin <omar_75@hotmail.dk>
2018-08-01 02:13:01 +0200

    Can now fetch data
    Can fetch top 5 from youtubehaiku, but the data is only visible in console

--- buttonAction.js
+++ buttonAction.js
@@ -2,4 +2,20 @@
   //alert("Hola!");
   document.getElementsByTagName('button')[14].click();
 
+  //We get the data from top haiku
+  //The data is written in json
+  //Only visible in console (Look for 'Object')
+  async function fetchTopFive() {
+  const URL = `https://www.reddit.com/r/youtubehaiku/top/.json?limit=5`; // Will return a 404
+  const fetchResult = fetch(URL)
+  const response = await fetchResult;
+  if (response.ok) {
+    const jsonData = await response.json();
+    console.log(jsonData);
+    } else {
+      throw Error(response.statusText);
+    }
+  }
+
+fetchTopFive();
 })();
